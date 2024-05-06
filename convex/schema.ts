96dd923024eae09475410fb9d2de2be5fd477146
Bezniak <ivan.bezniak@gmail.com>
2024-05-06 20:07:15 +0300

    4 commit - getMyConversations query added

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,10 +1,10 @@
-import {defineSchema, defineTable} from "convex/server";
-import {v} from "convex/values";
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
 
 export default defineSchema({
     users: defineTable({
         name: v.optional(v.string()),
-        email: v.optional(v.string()),
+        email: v.string(),
         image: v.string(),
         tokenIdentifier: v.string(),
         isOnline: v.boolean(),
@@ -16,5 +16,12 @@ export default defineSchema({
         groupName: v.optional(v.string()),
         groupImage: v.optional(v.string()),
         admin: v.optional(v.id("users")),
-    })
-})
+    }),
+
+    messages: defineTable({
+        conversation: v.id("conversations"),
+        sender: v.string(), // should be string so that it doesn't throw errors in openai part ("ChatGPT")
+        content: v.string(),
+        messageType: v.union(v.literal("text"), v.literal("image"), v.literal("video")),
+    }).index("by_conversation", ["conversation"]),
+});
